faa4c5527fc0ea8f6246f7db1c7d29c83e569417
Kacper <cyrankowskikacper123@gmail.com>
2025-08-13 22:24:08 +0200

    type

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,68 @@
-#include <ios>
 #include <iostream>
 #include <string>
+#include <array>
+
+std::array<std::string, 3> prefix = {"echo", "type", "exit"};
+
+enum class Commands
+{
+    EXIT,
+    ECHO,
+    TYPE,
+    NOT_FOUND
+};
+
+Commands command(const std::string& input)
+{
+    if(input.substr(0, prefix[2].size()) == prefix[2]) return Commands::EXIT;
+    else if (input.substr(0, prefix[0].size()) == prefix[0]) return Commands::ECHO;
+    else if (input.substr(0, prefix[1].size()) == prefix[1]) return Commands::TYPE;
+    else return Commands::NOT_FOUND;
+}
+
+bool is_shell_command(const std::string& cmd) 
+{
+    for(const std::string& c : prefix)
+    {
+        if(cmd == c.c_str())
+            return true;
+    }
+    return false;
+}
 
 int main() 
 {
     std::string input;
-    std::string prefix[1] = {"echo "};
 
-    std::cout << "$ ";
+    std::cout << "$ " << std::unitbuf;
 
     for(;;)
     {
         std::getline(std::cin, input);
 
-        if(input == "exit 0")
+        switch(command(input))
         {
-            return 0;
-        }
-        else if(input.substr(0, prefix[0].size()) == prefix[0])
-        {
-            std::cout << input.substr(prefix[0].size()) << std::endl;
-        }
-        else 
-        {
-            std::cout << input << ": command not found" << std::endl; 
+            case Commands::EXIT:
+                return 0;
+                break;
+        
+            case Commands::ECHO:
+                std::cout << input.substr(prefix[0].size() + 1) << std::endl;
+                break;
+
+            case Commands::TYPE:
+            {
+                std::string arg = input.substr(prefix[1].size() + 1);
+                if(is_shell_command(arg))
+                    std::cout << arg << " is a shell builtin" << std::endl;
+                else
+                    std::cout << arg << ": not found" << std::endl;
+                break;
+            }   
+
+            case Commands::NOT_FOUND:
+                std::cout << input << ": command not found" << std::endl;
+                break;
         }
 
         std::cout << "$ " << std::unitbuf;
