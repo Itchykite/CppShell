a23838f372ddad020e2af875bdd65b51f245da13
Kacper <cyrankowskikacper123@gmail.com>
2025-08-13 21:40:46 +0200

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,29 @@
 int main() 
 {
     std::string input;
+    std::string prefix[1] = {"echo "};
 
     std::cout << "$ ";
 
     for(;;)
     {
         std::getline(std::cin, input);
-        if (input == "exit 0")
-            break;
+
+        if(input == "exit 0")
+        {
+            std::cout << "Bye!" << std::endl;
+            return 0;
+        }
+        else if(input.substr(0, prefix[0].size()) == prefix[0])
+        {
+            std::cout << input.substr(prefix[0].size()) << std::endl;
+        }
         else 
-            std::cout << input << ": command not found" << std::endl;
+        {
+            std::cout << input << ": command not found" << std::endl; 
+        }
 
         std::cout << "$ " << std::unitbuf;
         std::cerr << std::unitbuf;
     }
-
-    return 0;
 }
